0fedbcec95c8a13b0ce38da9fd365a668feb32ba
jancarloa0524 <jancarloa0524@hstat.org>
2022-04-22 12:02:15 -0400

    added ability to write reports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore, collection, onSnapshot, setDoc, updateDoc, doc, query, where, arrayUnion } from 'firebase/firestore'
+import { getFirestore, collection, onSnapshot, setDoc, updateDoc, doc, arrayUnion, getDocs } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBq04h3lYW7lXPQN4rkE-wRdVdIF_V2oA4",
@@ -23,7 +23,7 @@ const colRef = collection(db, 'students')
 onSnapshot(colRef, (snapshot) => {
     let students = []
     snapshot.docs.forEach((doc) => {
-        students.push({ ...doc.data(), id: doc.id})
+        students.push({ ...doc.data(), id: doc.id })
     })
     console.log(students)
 })
@@ -59,3 +59,48 @@ dayForm.addEventListener('submit', (e) => {
     })
 })
 
+// Write reports:
+const reportForm = document.querySelector('.report')
+reportForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'students', reportForm.student.value)
+
+    updateDoc(docRef, {
+        report: reportForm.reportEntry.value
+    })
+      .then(() => {
+          reportForm.reset()
+      })
+})
+
+// Real-Time Table
+onSnapshot(colRef, (snapshot) => {
+    var table = document.querySelector('.table')
+
+    snapshot.docs.forEach((doc) => {
+        let row = document.createElement('tr')
+        table.appendChild(row)
+        if(doc.data().attendance != "") {
+            for (var j = 0; j <= 2; j++) {
+                let item = document.createElement('td')
+                if (j == 0){
+                    item.innerHTML = doc.data().name
+                } else if (j == 1) {
+                    item.innerHTML = doc.data().attendance.join(', ')
+                } else if (j == 2) {
+                    item.innerHTML = doc.data().report
+                }
+                row.appendChild(item)
+            }
+        }
+    })
+})
+
+// Plan for making/seeing student report
+/*
+First, users msut be able to write reports
+    In order to do this, I will allow users to be able to cycle through the users that they haven't written about yet
+Second, users must be able to see all data
+Use code found here https://jsbin.com/toquhopipa/edit?html,css,js,console,output as basis for creating the table
+*/
